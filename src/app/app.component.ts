d779b346e4b2b498f7f4bfec1c627ca9ad7c3389
Iurii Panarin <tduble94@gmail.com>
2020-10-08 23:50:54 +0200

    feat(init): commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+
+import { ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { BehaviorSubject, fromEvent } from 'rxjs';
+import { map, tap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'calendar';
+export class AppComponent implements OnInit {
+  heroName: string = "Hanzo"
+
+  heroFromInput: string
+
+  heroTimer: string = "bla";
+
+  @ViewChild('input') inputEl:ElementRef;
+
+  heroSubject$ = new BehaviorSubject("Tracer")
+
+  updateHeroName() {
+    this.heroName = "Mercy"
+    this.heroSubject$.next("Tank")
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.heroTimer += "!!"
+    }, 1000)
+  }
+
+  ngAfterViewInit() {
+    // Почему не работает хотя в html подписка есть(и как бы ты поправил чтобы работало)?
+   fromEvent(this.inputEl.nativeElement, 'keypress').pipe(
+     map((e: any) => e.target.value + "!"),
+   ).subscribe((value)=>{
+     this.heroFromInput = value
+     console.log(this.heroFromInput)
+   })
+   // Как бы ты переписал, можно менять html тоже ?(Подсказка FormControl)
+  }
+
 }
