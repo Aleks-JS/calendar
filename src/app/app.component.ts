47d4d5541b387a1d4cf3ecac9dab432471b003d8
Iurii Panarin <tduble94@gmail.com>
2020-10-14 09:54:11 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,44 @@
 
-import { ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { BehaviorSubject, fromEvent } from 'rxjs';
-import { map, tap } from 'rxjs/operators'
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { filter, map} from 'rxjs/operators'
+
+const years = [];
+
+for (let index = 1970; index<2030; index ++) {
+  years.push(index)
+}
+
+type Event = {
+  id: string;
+  startDate: Date;
+  endDate: Date
+  text: string;
+}
+
+// Массив событий на 14 октября 2020 в разное время
+// доп задание: сделать сервис(для получение списка эвентов )
+const events: Array<Event> = [
+  {
+    id: "1",
+    startDate: new Date(1602644400000), // 5-7
+    endDate: new Date(1602651600000),
+    text: "Выкинуть мусор"
+  },
+  {
+    id: "2",
+    startDate: new Date(1602676800000), // 14-15
+    endDate: new Date(1602680400000),
+    text: "Посмотреть телевизор"
+  },
+  {
+    id: "3",
+    startDate: new Date(1602698400000), // 20-22
+    endDate: new Date(1602705600000),
+    text: "Послушать подкаст"
+  }
+]
 
 @Component({
   selector: 'app-root',
@@ -9,37 +46,43 @@ import { map, tap } from 'rxjs/operators'
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit {
-  heroName: string = "Hanzo"
+export class AppComponent {
 
-  heroFromInput: string
+  allYears = years
 
-  heroTimer: string = "bla";
+  yearsControl = new FormControl((new Date).getFullYear())
 
-  @ViewChild('input') inputEl:ElementRef;
+  currentYear$ = this.yearsControl.valueChanges.pipe(
+    filter((value) => parseInt(value).toString() == value),
+    map((value) => parseInt(value))
+  )
 
-  heroSubject$ = new BehaviorSubject("Tracer")
+  currentDay$ = new Subject()
 
-  updateHeroName() {
-    this.heroName = "Mercy"
-    this.heroSubject$.next("Tank")
+  // Сделать так чтобы при выборе месяца и года показывалось правильное число дней
+  // Подсказка: смотри combineLatest
+
+  dayInMonth$ = this.currentYear$.pipe(
+    map((value) => {
+      if (value % 2 == 0) {
+        return Array(15).fill(0).map((_, index) => index)
+      }
+      return Array(31).fill(0).map((_, index) => index)
+    })
+  )
+
+  // Сделать список месяцев
+  setDay(day: number) {
+    this.currentDay$.next(day)
   }
 
-  ngOnInit() {
-    setInterval(() => {
-      this.heroTimer += "!!"
-    }, 1000)
+  // Меняет на предыдущий месяц
+  prevMonth() {
+
   }
 
-  ngAfterViewInit() {
-    // Почему не работает хотя в html подписка есть(и как бы ты поправил чтобы работало)?
-   fromEvent(this.inputEl.nativeElement, 'keypress').pipe(
-     map((e: any) => e.target.value + "!"),
-   ).subscribe((value)=>{
-     this.heroFromInput = value
-     console.log(this.heroFromInput)
-   })
-   // Как бы ты переписал, можно менять html тоже ?(Подсказка FormControl)
-  }
+  // Меняет на след месяц
+  nextMonth() {
 
+  }
 }
