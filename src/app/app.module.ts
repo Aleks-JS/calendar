70c858b5af09ba306e37e7ed5389da74d9d4db9d
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-10-22 07:51:12 +0300

    basic table cell styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 
+
 @NgModule({
   declarations: [
     AppComponent
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select'
+import { MatDialogModule } from '@angular/material/dialog';
 
 const MaterialComponents = [
-  MatButtonModule
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatListModule,
+  MatDividerModule,
+  MatSelectModule,
+  MatDialogModule
 ]
 
 @NgModule({
